73533d2825c766b4e59c1a9f8dee0571364457ae
Joao <joao_amoury@hotmail.com>
2022-02-16 22:59:52 -0300

    removido do loadmany Usercontroller  o userType  e profile, pois ja esta no static boot do model

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -10,7 +10,7 @@ class UserController {
 
     async show({ params }){
         const user = await User.findOrFail(params.id)
-        await user.loadMany(['userType', 'profile', 'permissions', 'roles'])
+        await user.loadMany(['permissions', 'roles'])
         return user
     }
 
